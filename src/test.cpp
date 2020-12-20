2952ba9569414982d96686f2211d3f7cfa99a0eb
itrooz <42669835+iTrooz@users.noreply.github.com>
2020-12-20 09:10:44 +0100

    a

---
+++ src/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by itrooz on 19/12/2020.
+//
+
+#include "test.h"
---
+++ src/test.h
@@ -0,0 +1,14 @@
+//
+// Created by itrooz on 19/12/2020.
+//
+
+#ifndef TEST_TEST_H
+#define TEST_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //TEST_TEST_H
